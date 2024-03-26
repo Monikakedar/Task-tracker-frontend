92f3435daa8e96e6137806cd4eb900e780294be9
Monika kedar <monikakedark@gmail.com>
2024-03-26 20:07:17 +0530

    Add structure for frontend

---
+++ task-frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './Compoent/Footer';
+import Header from './Compoent/Header';
+import Home from './Pages/Home';
+
+let  App=() =>{
+  return (
+    <div className="App">
+      <h1>welcome</h1>
+     <Header></Header>
+     <Footer></Footer>
+     <Home></Home>
+       </div>
+  );
+}
+
+export default App;
